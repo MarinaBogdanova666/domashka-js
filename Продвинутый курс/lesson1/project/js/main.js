9e00a86697d9087465b011d117c54705cdf1e35c
Marina Bogdanova <seilsoul13@gmail.com>
2021-04-03 15:54:27 +0500

    переделала дз 2

--- Продвинутый курс/lesson1/project/js/main.js
+++ Продвинутый курс/lesson1/project/js/main.js
@@ -1,26 +1,65 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-    {id: 5},
-];
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this.fetchProducts();
+    }
+    
+    fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    render() {
+        const blockItem = document.querySelector(this.container);
+        for(let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            blockItem.insertAdjacentHTML('beforeend',productObj.render())
+        }
+    }
+    getSum() {
+        let resultSum = this.allProducts.reduce((sum, item) => sum += item.price, 0);
+        alert(resultSum);
+    }    
+}
 
-//Функция для формирования верстки каждого товара
 
-// Добавила значения по умолчанию для аргументов функции.
-const renderProduct = (title = "товар не найден", price = "уточните у оператора") => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+class ProductItem {
+	constructor(product, img = 'https://via.placeholder.com/150'){
+		this.title = product.title;
+		this.price = product.price;
+		this.id = product.id;
+		this.img = img;
+	}
+
+	render() {
+		 return `<div class="product-item" data-id="${this.id}">
+                <h3>${this.title}</h3>
+                <img src="${this.img}">
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    // Убрали запятые с помощью метода .join
-    const productsList = list.map(item => renderProduct(item.title, item.price)).join("");
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
-};
+	}
+}
 
-renderPage(products);
+let list = new ProductsList();
+list.render();
+list.getSum();
+
+class cart {
+    addProduct() {
+    }
+    removeProduct() {
+    }
+    changeProduct() {
+    }
+}
+
+class elemCart {
+
+}
